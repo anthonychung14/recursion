192474e26676884d26ac8113a96aa357ea2534e6
Anthony Chung <anthonychung14@gmail.com>
2015-10-21 16:59:30 -0700

    StringyJSON complete. Needs refactoring to be cleaner

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,53 +4,11 @@
 // };
 
 // But instead we're going to implement it from scratch:
-/*
-  This code is supposd to gather all elements of "classname" 
-  into an array
 
-  1. Why don't you first see how it works natively
-    - Creates an array of HTML elements that have the same "class"
-    - All the HTML formatting remains intact
-    - One big array that can be set to another variable
+//  This code is supposd to gather all elements of "classname" 
+//  into an array
 
-  2. Then, create an understanding of what each of the pieces are.
-    - Document.body => returns the whole thing. This is the DOM
-    
-    - Node.childNodes => returns a live collection of childNodes
-      -Ordered collection that was children of current element.
-      -I think you'll use this to return everything under the DIV class
-    
-    -Element.classList => returns live DOMToken. 
-      -This will let you check for classlists
-      -el.classList(contains("foo"))
-
-  3. Use Twittler to play around with DOM elements and test them out
-
-    -right now, I have figured out how to test that a class exists
-
-
-  4. Figure out what the base case is supposed to be
-
-    -how can I iteratively reduce
-
-    if ()
-
-
-
-  5. Base case = return all the elements
-
-
-  6. Else, call the function again, but this time with a slight change
-
-      So it looks like you add everything under the given class name UP
-      until a </div>
-
-
-  7. If there are none there at all, you should return some kind of alert
-
-  */
 var getElementsByClassName = function(className){
-  // your code here
   var docBody = document.body;
   var result = [];
   var walk = function (node, func) {
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,55 @@
 
 // but you don't so you're going to write it from scratch:
 
+//what I don't know is how to typecast brackets or parens
+
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  var str = ''
+  //base case:
+  if (typeof obj === 'string') {
+    str += '"' + String(obj) + '"';
+  }
+  else if (typeof obj === 'boolean') {
+    str += String(obj)
+  }
+  else if (typeof obj === 'number') {
+    str += String(obj);
+  }
+  else if (obj === null) {
+    str += String(obj);
+  }
+  else if (Array.isArray(obj)) {
+    str += '[';
+    for (var i = 0; i <obj.length; i++) {
+      if (i<obj.length-1) 
+          {str += stringifyJSON(obj[i]) + ","}
+      else if (i === obj.length-1) 
+          {str+= stringifyJSON(obj[i])}
+    }
+    str += "]"
+  }
+  else if (typeof obj === 'object') {
+    str += "{"
+    for (var p in obj) {
+      if (p === "functions" || p === undefined) {
+        str += "}"
+        return str;
+      }
+      if (obj.hasOwnProperty(p)) { 
+        if (p === Object.keys(obj)[Object.keys(obj).length-1]) {
+          str += '"' + String(p) + '"' + ':' + stringifyJSON(obj[p])
+        }
+        else {
+          str += '"' + String(p) + '"' +':' + stringifyJSON(obj[p]) + ",";}        
+      }      
+    } 
+  str+= "}"
+  }
+  return str;
+}
+
+
+
+
+
+ 
